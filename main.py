4baa31adf3e437000b763d081784f3f72f620949
Orestis <orestispanago@gmail.com>
2023-06-13 17:47:44 +0300

    Using YYYYMMDD_HHMMSS folder and file name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import shutil
 
-from camera import CamClient, exp_api_dict
+from camera import CamClient, exp_api_dict, exposure_times
 from nas import FTPClient
 
 cam_ip = "YourCameraIP"
@@ -16,20 +16,19 @@ ftp_share = "YourFTPShare"
 
 
 now = datetime.datetime.utcnow()
-local_folder = now.strftime("%Y/%m/%d")
-if not os.path.exists(local_folder):
-    os.makedirs(local_folder)
+folder_name = now.strftime("%Y%m%d_%H%M%S")
+date_time = now.strftime("%Y%m%d_%H%M%S")
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
 cam_client = CamClient(cam_ip, cam_user, cam_pass)
 
-exposure_values = ["1_16000", "1_4000", "1_2000", "1_500", "1_250"]
-
-for exp in exposure_values:
+for exp in reversed(exposure_times):
     cam_client.set_exposure(exp_api_dict.get(exp))
-    cam_client.download_img(f"{local_folder}/{now.strftime('%H%M')}_exp_{exp}.png")
+    cam_client.download_img(f"{folder_name}/{date_time}_exp_{exp}.png")
 cam_client.reset_factory()
 
 with FTPClient(ftp_ip, ftp_user, ftp_password, ftp_share) as ftp_client:
-    ftp_client.upload_folder(local_folder)
+    ftp_client.upload_folder(folder_name)
 
-shutil.rmtree(local_folder)
+# shutil.rmtree(folder_name)
